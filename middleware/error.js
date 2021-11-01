01c016a771626e21c7f9fe45ac5aba7eecbf2ed0
Muntasir-Mahmud <m.siyam529@gmail.com>
2021-11-02 03:19:53 +0600

    Added Custome error handler for duplicate and validation error

--- middleware/error.js
+++ middleware/error.js
@@ -5,12 +5,25 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message
 
     // Log to console for the developer
-    console.log(err.stack);
+    console.log(err);
 
     // Mongoose bad ObjectId
     if (err.name === 'CastError') {
         const message = `Resource not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404) 
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicated field value entered'
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message); 
+        // UP : here we get all the values of all err.errors
+        error = new ErrorResponse(message, 400)
     }
 
     res.status(error.statusCode || 500).json({
