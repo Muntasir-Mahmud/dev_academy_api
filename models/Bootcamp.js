fec91f719cb1759e5ffd211217cc31667a48443f
Muntasir-Mahmud <m.siyam529@gmail.com>
2021-11-02 03:58:06 +0600

    Added slugify to create slug

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -97,5 +98,12 @@ const BootcampSchema = new mongoose.Schema({
       default: Date.now
     }
 })
+
+// Create bootcamp slug from name
+BootcampSchema.pre('save', function(next) {
+  // UP: we do not use arrow function here , that use scope differently
+  this.slug = slugify(this.name, { lower: true })
+  next();
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
