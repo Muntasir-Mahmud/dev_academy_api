cea60be825ee1e4914627db05b006aed5ef4d4de
Muntasir-Mahmud <m.siyam529@gmail.com>
2021-11-01 15:29:19 +0600

    Exported the model

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -95,15 +95,7 @@ const BootcamoSchema = new mongoose.Schema({
     createdAt: {
       type: Date,
       default: Date.now
-    },
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: true
     }
-},
-{
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true } 
-},
-)
+})
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
